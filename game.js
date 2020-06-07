49629c8a36606d3b7e8f99aeab246bba06ca3a7d
NickoValentino <nicko.valentinoo@gmail.com>
2020-06-07 10:42:26 +0800

    Added PLAY AGAIN button after you lose, refactor code

--- game.js
+++ game.js
@@ -1,10 +1,11 @@
 
-//DOM ELEMENT
+//DOM ELEMENTS
 const elemMessageBox = document.getElementById(`message-box`);
 const elemParagraphs = elemMessageBox.getElementsByTagName(`p`);
 const elemStageInfo = document.getElementById(`stage-info`);
 const stageInfoText = elemStageInfo.getElementsByTagName(`h1`);
 const elemNextButton = document.getElementById(`next-stage`);
+const elemPlayAgainButton = document.getElementById(`play-again`);
 
 
 const elemPlayerInfo = document.getElementById(`player-info`);
@@ -13,47 +14,75 @@ const elemBattleOption = document.getElementById(`battle-option`);
 const elemEnemySprite = document.getElementById(`monster-sprite`);
 
 
-const playerName = elemPlayerInfo.getElementsByClassName(`name`);
-const playerHpText = elemPlayerInfo.getElementsByClassName(`hp-text`);
-const playerHpBar = elemPlayerInfo.getElementsByClassName(`bar`);
+const elemPlayerNames = elemPlayerInfo.getElementsByClassName(`name`);
+const elemPlayerHpTexts = elemPlayerInfo.getElementsByClassName(`hp-text`);
+const elemPlayerHpBars = elemPlayerInfo.getElementsByClassName(`bar`);
 
-const enemyName = elemEnemyInfo.getElementsByClassName(`name`);
-const enemyHpText = elemEnemyInfo.getElementsByClassName(`hp-text`);
-const enemyHpBar = elemEnemyInfo.getElementsByClassName(`bar`);
-const enemySpritesBody = elemEnemySprite.getElementsByClassName(`monster-body`);
-const enemyBaloon = document.getElementById(`monster-baloon`)
-//DOM ELEMENT
+const elemEnemyNames = elemEnemyInfo.getElementsByClassName(`name`);
+const elemEnemyHpTexts = elemEnemyInfo.getElementsByClassName(`hp-text`);
+const elemEnemyHpBars = elemEnemyInfo.getElementsByClassName(`bar`);
+const elemEnemySpritesBody = elemEnemySprite.getElementsByClassName(`monster-body`);
+const elemEnemyBaloon = document.getElementById(`monster-baloon`)
+//DOM ELEMENTS
 
+
+
+//SETTING HERE
+const settingPlayerBaseHp = 20;
+const settingEnemyBaseHp = 3;
+const settingEnemyName = `Enemy`;
+const settingPlayerDefaultName = `Player`;
+//SETTING HERE
+
+//Variables
+let currentStage;
+
+let playerBaseHp;
+let playerDefaultName;
+let playerValue;
+
+let enemyBaseHp;
+let enemyDefaultName;
+let enemyValue;
+let enemyMessages;
+
+let objectPlayer;
+let objectEnemy;
+//Variables
 
 //Initial database
-let currentStage = 0;
-
-let playerBaseHp = 20;
-let playerDefaultName = `Player`;
-let playerValue = null;
-
-let enemyBaseHp = 3;
-let enemyDefaultName = `Enemy`;
-let enemyValue = null;
-let enemyMessages = [`?`,`Rock`,`Paper`,`Scissors`];
-
-let objectPlayer = {};
-let objectEnemy = {};
-
 let greenColor = `#4caf50`;
 let yellowColor = `yellow`;
 let redColor = `red`;
 let blueColor = `blue`;
-
-let idleMessage;
 //Initial database
+
+function initializeBaseValue() {
+    currentStage = 0;
+
+    playerBaseHp = settingPlayerBaseHp;
+    playerDefaultName = settingPlayerDefaultName;
+    playerValue = null;
+
+    enemyBaseHp = settingEnemyBaseHp;
+    enemyDefaultName = settingEnemyName;
+    enemyValue = null;
+    enemyMessages = [`?`, `Rock`, `Paper`, `Scissors`];
+
+    objectPlayer = {};
+    objectEnemy = {};
+}
 
 function initializePlayer(player) {
 
     player.name = playerDefaultName;
     player.maxHp = playerBaseHp;
     player.currHp = player.maxHp;
-    playerName[0].innerHTML = player.name;
+    player.elemTextName = elemPlayerNames[0];
+    player.elemTextHp = elemPlayerHpTexts[0];
+    player.elemBarHp = elemPlayerHpBars[0];
+
+    player.elemTextName.innerHTML = player.name;
 }
 
 function initializeEnemy(enemy) {
@@ -61,79 +90,92 @@ function initializeEnemy(enemy) {
     enemy.name = enemyDefaultName;
     enemy.maxHp = enemyBaseHp;
     enemy.currHp = enemy.maxHp;
-    enemySpritesBody[0].style.visibility = `visible`;
-    enemyName[0].innerHTML = enemy.name;
-    enemySpritesBody[0].style.filter = `none`;
+    enemy.elemTextName = elemEnemyNames[0];
+    enemy.elemTextHp = elemEnemyHpTexts[0];
+    enemy.elemBarHp = elemEnemyHpBars[0];
+    enemy.elemSprite = elemEnemySpritesBody[0];
+
+    enemy.elemTextName.innerHTML = enemy.name;
+
+    enemy.elemSprite.style.visibility = `visible`;
+    enemy.elemSprite.style.filter = `none`;
 }
 
-function updateHpInfo() {
-    playerHpText[0].innerHTML = `HP: ${objectPlayer.currHp}/${objectPlayer.maxHp}`;
-    playerHpBar[0].style.width = `${(objectPlayer.currHp / objectPlayer.maxHp) * 100}%`;
-    if ((objectPlayer.currHp / objectPlayer.maxHp * 100) > 50) {
-        playerHpBar[0].style.backgroundColor = greenColor;
-    }
-    else if ((objectPlayer.currHp / objectPlayer.maxHp * 100) > 20) {
-        playerHpBar[0].style.backgroundColor = yellowColor;
-    }
-    else {
-        playerHpBar[0].style.backgroundColor = redColor;
-    }
-
-
-    enemyHpText[0].innerHTML = `HP: ${objectEnemy.currHp}/${objectEnemy.maxHp}`;
-    enemyHpBar[0].style.width = `${(objectEnemy.currHp / objectEnemy.maxHp) * 100}%`;
-
-    if ((objectEnemy.currHp / objectEnemy.maxHp * 100) > 50) {
-        enemyHpBar[0].style.backgroundColor = greenColor;
+//Update UI information about HP (text and bar)
+function updateHpInfo(objectUnit) {
+    objectUnit.elemTextHp.innerHTML = `HP: ${objectUnit.currHp}/${objectUnit.maxHp}`;
+    objectUnit.elemBarHp.style.width = `${(objectUnit.currHp / objectUnit.maxHp) * 100}%`;
+    updateHpBarColor(objectUnit);
+}
+function updateHpBarColor(objectUnit) {
+    if ((objectUnit.currHp / objectUnit.maxHp * 100) > 50) {
+        objectUnit.elemBarHp.style.backgroundColor = greenColor;
     }
     else if ((objectEnemy.currHp / objectEnemy.maxHp * 100) > 20) {
-        enemyHpBar[0].style.backgroundColor = yellowColor;
+        objectUnit.elemBarHp.style.backgroundColor = yellowColor;
     }
     else {
-        enemyHpBar[0].style.backgroundColor = redColor;
+        objectUnit.elemBarHp.style.backgroundColor = redColor;
     }
-
 }
-function translateToText(value){
+function translateChoiceToText(value) {
     let text = ``;
-    if(value === 1){
-        text=`Rock`;
+    if (value === 1) {
+        text = `Rock`;
     }
-    else if(value === 2){
-        text=`Paper`;
+    else if (value === 2) {
+        text = `Paper`;
     }
-    else if(value === 3){
-        text= `Scissors`;
+    else if (value === 3) {
+        text = `Scissors`;
     }
 
     return text;
 }
-function damage(result,playerValue,enemyValue) {
-    let playerChoice = translateToText(playerValue);
-    let enemyChoice = translateToText(enemyValue);
+
+function setCombatMessage(result, playerValue, enemyValue) {
+    let playerChoice = translateChoiceToText(playerValue);
+    let enemyChoice = translateChoiceToText(enemyValue);
 
     let combatMessage = `You pick ${playerChoice}\n Enemy pick ${enemyChoice} \n\n`;
     let winMessage = `You win! deal 1 damage to enemy`;
     let loseMessage = `You lose! take 1 damage`;
     let drawMessage = `It's a draw!`;
+
     if (result === 1) {
-        objectEnemy.currHp -= 1
-        if (objectEnemy.currHp < 0) { objectEnemy.currHp = 0 };
-        updateMessageBox(combatMessage + winMessage);
+        return combatMessage + winMessage;
     }
     else if (result === 2) {
-        objectPlayer.currHp -= 1
-        if (objectPlayer.currHp < 0) { objectPlayer.currHp = 0 };
-        updateMessageBox(combatMessage + loseMessage);
+        return combatMessage + loseMessage;
     }
     else {
-        updateMessageBox(combatMessage + drawMessage)
+        return combatMessage + drawMessage;
     }
-    updateHpInfo();
+}
+function damage(result, playerValue, enemyValue) {
+
+    let target = null;
+
+    if (result === 1) {
+        target = objectEnemy;
+        objectEnemy.currHp -= 1
+        if (objectEnemy.currHp < 0) { objectEnemy.currHp = 0 };
+    }
+    else if (result === 2) {
+        target = objectPlayer;
+        objectPlayer.currHp -= 1
+        if (objectPlayer.currHp < 0) { objectPlayer.currHp = 0 };
+    }
+
+    //Directly update message and info on UI after damage calculation
+    updateMessageBox(setCombatMessage(result, playerValue, enemyValue));
+    if (target !== null) {
+        updateHpInfo(target);
+    }
 }
 
-function checkEnemyHpBoost(){
-    if(currentStage % 3 === 0){
+function checkEnemyHpBoost() {
+    if (currentStage % 3 === 0) {
         enemyBaseHp++;
     }
 }
@@ -141,8 +183,8 @@ function updateMessageBox(message) {
     elemParagraphs[0].innerText = message;
 
 }
-function updateEnemyMessage(message){
-    enemyBaloon.innerHTML = message;
+function updateEnemyMessage(message) {
+    elemEnemyBaloon.innerHTML = message;
 }
 
 function updateStage() {
@@ -156,8 +198,8 @@ function enemyRandomChoices(numOfChoices) {
 
     return choices;
 }
-function enemyDeadEffect(){
-    enemySpritesBody[0].style.filter = `grayscale(100%)`;
+function enemyDeadEffect() {
+    elemEnemySpritesBody[0].style.filter = `grayscale(100%)`;
 }
 function checkResult() {
     if (playerValue == enemyValue) {
@@ -186,37 +228,38 @@ function checkResult() {
     }
 }
 
-function gameFlow(e){
+function gameFlow(e) {
     playerValue = parseInt(e.currentTarget.value);
     enemyValue = enemyRandomChoices(3);
     let result = checkResult();
-    damage(result,playerValue,enemyValue);
+    damage(result, playerValue, enemyValue);
     checkEndRound();
 
 }
-function checkEndRound(){
+function checkEndRound() {
     if (objectEnemy.currHp === 0) {
         hideBattleOptions();
         enemyDeadEffect();
-        showNextStageButton();
+        showButton(elemNextButton);
         updateMessageBox(`YOU WIN!`);
     }
     else if (objectPlayer.currHp === 0) {
         hideBattleOptions();
+        showButton(elemPlayAgainButton);
         updateMessageBox(`YOU LOSE!
         Final Score: ${currentStage}`);
     }
 }
-function showNextStageButton() {
-        elemNextButton.style.visibility = `visible`;
-        elemNextButton.style.display = `inline`;
-        elemNextButton.disabled = false;
-    
+function showButton(element) {
+    element.style.visibility = `visible`;
+    element.style.display = `inline`;
+    element.disabled = false;
+
 }
 
-function hideNextStageButton(){
-    elemNextButton.style.display = `none`;
-    elemNextButton.disabled = true;
+function hideButton(element) {
+    element.style.display = `none`;
+    element.disabled = true;
 }
 
 function addBattleOptionButtonsListener() {
@@ -232,37 +275,44 @@ function hideBattleOptions() {
     elemBattleOption.style.display = `none`;
 
 }
-function showBattleOptions(){
+function showBattleOptions() {
     elemBattleOption.style.visibility = `visible`;
     elemBattleOption.style.display = `flex`;
 }
 function initializeStage() {
     //Initialize Round
-    updateStage();
-    checkEnemyHpBoost();
-    initializeEnemy(objectEnemy);
-    updateHpInfo();
-    hideNextStageButton();
-    showBattleOptions();
+    updateStage();      //Increment stage variable and update stage text
+    checkEnemyHpBoost();    //Check if enemy base HP got a boost
+    initializeEnemy(objectEnemy); //Initialize the enemy with it's value (hp/sprite)
+    updateHpInfo(objectPlayer); //update HP info in UI
+    updateHpInfo(objectEnemy); //update HP info in UI
+    hideButton(elemNextButton);  //hide next stage button (next stage only shown after you win the stage)
+    hideButton(elemPlayAgainButton);
+    showBattleOptions();    //Show your RPS buttons
     updateEnemyMessage(enemyMessages[0]);
     updateMessageBox(`Choose your option`);
     //Initialize Round
 }
+function initializeButtonsListener(){
+    elemNextButton.addEventListener('click', initializeStage);
+    elemPlayAgainButton.addEventListener(`click`, startGame);
+    addBattleOptionButtonsListener();
+}
+function startGame() {
+    //Set starting value for all variables
+    initializeBaseValue();
+    //Initialize Player data
+    initializePlayer(objectPlayer);
+    //Initialize Player data
 
-
+    //Start round!
+    initializeStage();
+    //Start round!
+}
 //===============================GAME START HERE==============================
-//Add listener to all buttons
-elemNextButton.addEventListener('click' ,initializeStage);
-addBattleOptionButtonsListener();
-//Add listener to all buttons
+initializeButtonsListener();
+startGame();
 
-//Initialize Player data
-initializePlayer(objectPlayer);
-//Initialize Player data
-
-//Start round!
-initializeStage();
-//Start round!
 //===============================GAME END HERE==============================
 
 
